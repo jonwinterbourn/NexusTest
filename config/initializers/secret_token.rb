2ee4c189febfced54a8af5e70b55c14297677681
Jon Winterbourn <jonwinterbourn@gmail.com>
2013-02-08 13:19:21 +0000

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NexusTest::Application.config.secret_token = 'aaf77de6caa3057330b6abb5256651008a2ae98941b12821d420dad44d461daa29d1e5e97588ad8ddb042e9bdf6ab66760bb54ec988c35f04635ad57b2017a55'

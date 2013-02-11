e59c8a56f408f7e03500fa58e23ccdd757bd798b
Jon Winterbourn <jonwinterbourn@gmail.com>
2013-02-11 17:19:06 +0000

    try again...

---
+++ app/controllers/course_search_controller.rb
@@ -0,0 +1,5 @@
+class CourseSearchController < ApplicationController
+  def search
+  end
+
+end
---
+++ db/migrate/20130211164411_create_courses.rb
@@ -0,0 +1,11 @@
+class CreateCourses < ActiveRecord::Migration
+  def change
+    create_table :courses do |t|
+      t.integer :id
+      t.string :name
+      t.text :summary
+
+      t.timestamps
+    end
+  end
+end
